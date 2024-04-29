8e726408cc480df24fbb6eb03dd3241c40162c0c
Ninjdai <ninjdai@duck.com>
2024-04-29 09:45:02 +0200

    feat: Add Grapejuice fluids

--- common/src/main/java/dev/ninjdai/doaddoncompat/registry/DoAddonBlocks.java
+++ common/src/main/java/dev/ninjdai/doaddoncompat/registry/DoAddonBlocks.java
@@ -24,6 +24,16 @@ public class DoAddonBlocks {
     public static final RegistryEntry<Block> GOAT_MILK_BLOCK = BLOCKS.register("goat_milk", () -> new BotariumLiquidBlock(DoAddonFluidProperties.GOAT_MILK, BlockBehaviour.Properties.copy(Blocks.WATER).mapColor(MapColor.TERRACOTTA_WHITE)));
     public static final RegistryEntry<Block> WARPED_MILK_BLOCK = BLOCKS.register("warped_milk", () -> new BotariumLiquidBlock(DoAddonFluidProperties.WARPED_MILK, BlockBehaviour.Properties.copy(Blocks.WATER).mapColor(MapColor.WARPED_WART_BLOCK)));
 
+    public static final RegistryEntry<Block> RED_GRAPEJUICE_BLOCK = BLOCKS.register("red_grapejuice", () -> new BotariumLiquidBlock(DoAddonFluidProperties.RED_GRAPEJUICE, BlockBehaviour.Properties.copy(Blocks.WATER).mapColor(MapColor.COLOR_RED)));
+    public static final RegistryEntry<Block> WHITE_GRAPEJUICE_BLOCK = BLOCKS.register("white_grapejuice", () -> new BotariumLiquidBlock(DoAddonFluidProperties.WHITE_GRAPEJUICE, BlockBehaviour.Properties.copy(Blocks.WATER).mapColor(MapColor.COLOR_LIGHT_GREEN)));
+    public static final RegistryEntry<Block> JUNGLE_RED_GRAPEJUICE_BLOCK = BLOCKS.register("jungle_red_grapejuice", () -> new BotariumLiquidBlock(DoAddonFluidProperties.JUNGLE_RED_GRAPEJUICE, BlockBehaviour.Properties.copy(Blocks.WATER).mapColor(MapColor.COLOR_RED)));
+    public static final RegistryEntry<Block> JUNGLE_WHITE_GRAPEJUICE_BLOCK = BLOCKS.register("jungle_white_grapejuice", () -> new BotariumLiquidBlock(DoAddonFluidProperties.JUNGLE_WHITE_GRAPEJUICE, BlockBehaviour.Properties.copy(Blocks.WATER).mapColor(MapColor.COLOR_LIGHT_GREEN)));
+    public static final RegistryEntry<Block> SAVANNA_RED_GRAPEJUICE_BLOCK = BLOCKS.register("savanna_red_grapejuice", () -> new BotariumLiquidBlock(DoAddonFluidProperties.SAVANNA_RED_GRAPEJUICE, BlockBehaviour.Properties.copy(Blocks.WATER).mapColor(MapColor.COLOR_RED)));
+    public static final RegistryEntry<Block> SAVANNA_WHITE_GRAPEJUICE_BLOCK = BLOCKS.register("savanna_white_grapejuice", () -> new BotariumLiquidBlock(DoAddonFluidProperties.SAVANNA_WHITE_GRAPEJUICE, BlockBehaviour.Properties.copy(Blocks.WATER).mapColor(MapColor.COLOR_LIGHT_GREEN)));
+    public static final RegistryEntry<Block> TAIGA_RED_GRAPEJUICE_BLOCK = BLOCKS.register("taiga_red_grapejuice", () -> new BotariumLiquidBlock(DoAddonFluidProperties.RED_GRAPEJUICE, BlockBehaviour.Properties.copy(Blocks.WATER).mapColor(MapColor.COLOR_RED)));
+    public static final RegistryEntry<Block> TAIGA_WHITE_GRAPEJUICE_BLOCK = BLOCKS.register("taiga_white_grapejuice", () -> new BotariumLiquidBlock(DoAddonFluidProperties.WHITE_GRAPEJUICE, BlockBehaviour.Properties.copy(Blocks.WATER).mapColor(MapColor.COLOR_LIGHT_GREEN)));
+    public static final RegistryEntry<Block> CRIMSON_GRAPEJUICE_BLOCK = BLOCKS.register("crimson_grapejuice", () -> new BotariumLiquidBlock(DoAddonFluidProperties.CRIMSON_GRAPEJUICE, BlockBehaviour.Properties.copy(Blocks.WATER).mapColor(MapColor.CRIMSON_HYPHAE)));
+    public static final RegistryEntry<Block> WARPED_GRAPEJUICE_BLOCK = BLOCKS.register("warped_grapejuice", () -> new BotariumLiquidBlock(DoAddonFluidProperties.WARPED_GRAPEJUICE, BlockBehaviour.Properties.copy(Blocks.WATER).mapColor(MapColor.WARPED_HYPHAE)));
 
     public static void registerExtraProperties() {}
 }
