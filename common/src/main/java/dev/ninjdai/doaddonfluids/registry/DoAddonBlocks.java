e4eef7a2442a5ca0fb9c157cfeb4259828b500bf
Ninjdai <ninjdai@duck.com>
2024-05-28 10:52:49 +0200

    temporary comment out of Meadow milks

--- common/src/main/java/dev/ninjdai/doaddonfluids/registry/DoAddonBlocks.java
+++ common/src/main/java/dev/ninjdai/doaddonfluids/registry/DoAddonBlocks.java
@@ -17,12 +17,12 @@ public class DoAddonBlocks {
     public static final ResourcefulRegistry<Block> TRANSPARENT_BLOCKS = ResourcefulRegistries.create(BLOCKS);
 
     //public static final RegistryEntry<Block> YEAST_BLOCK = BLOCKS.register("yeast", () -> new LavaLikeLiquidBlock(DoAddonFluidProperties.YEAST, BlockBehaviour.Properties.copy(Blocks.LAVA).mapColor(MapColor.COLOR_BROWN)));
-    public static final RegistryEntry<Block> BUFFALO_MILK_BLOCK = BLOCKS.register("buffalo_milk", () -> new BotariumLiquidBlock(DoAddonFluidProperties.BUFFALO_MILK, BlockBehaviour.Properties.copy(Blocks.WATER).mapColor(MapColor.TERRACOTTA_WHITE)));
+    /*public static final RegistryEntry<Block> BUFFALO_MILK_BLOCK = BLOCKS.register("buffalo_milk", () -> new BotariumLiquidBlock(DoAddonFluidProperties.BUFFALO_MILK, BlockBehaviour.Properties.copy(Blocks.WATER).mapColor(MapColor.TERRACOTTA_WHITE)));
     public static final RegistryEntry<Block> AMETHYST_MILK_BLOCK = BLOCKS.register("amethyst_milk", () -> new BotariumLiquidBlock(DoAddonFluidProperties.AMETHYST_MILK, BlockBehaviour.Properties.copy(Blocks.WATER).mapColor(MapColor.TERRACOTTA_PURPLE)));
     public static final RegistryEntry<Block> SHEEP_MILK_BLOCK = BLOCKS.register("sheep_milk", () -> new BotariumLiquidBlock(DoAddonFluidProperties.SHEEP_MILK, BlockBehaviour.Properties.copy(Blocks.WATER).mapColor(MapColor.TERRACOTTA_WHITE)));
     public static final RegistryEntry<Block> GRAIN_MILK_BLOCK = BLOCKS.register("grain_milk", () -> new BotariumLiquidBlock(DoAddonFluidProperties.GRAIN_MILK, BlockBehaviour.Properties.copy(Blocks.WATER).mapColor(MapColor.COLOR_BROWN)));
     public static final RegistryEntry<Block> GOAT_MILK_BLOCK = BLOCKS.register("goat_milk", () -> new BotariumLiquidBlock(DoAddonFluidProperties.GOAT_MILK, BlockBehaviour.Properties.copy(Blocks.WATER).mapColor(MapColor.TERRACOTTA_WHITE)));
-    public static final RegistryEntry<Block> WARPED_MILK_BLOCK = BLOCKS.register("warped_milk", () -> new BotariumLiquidBlock(DoAddonFluidProperties.WARPED_MILK, BlockBehaviour.Properties.copy(Blocks.WATER).mapColor(MapColor.WARPED_WART_BLOCK)));
+    public static final RegistryEntry<Block> WARPED_MILK_BLOCK = BLOCKS.register("warped_milk", () -> new BotariumLiquidBlock(DoAddonFluidProperties.WARPED_MILK, BlockBehaviour.Properties.copy(Blocks.WATER).mapColor(MapColor.WARPED_WART_BLOCK)));*/
 
     public static final RegistryEntry<Block> RED_GRAPEJUICE_BLOCK = BLOCKS.register("red_grapejuice", () -> new BotariumLiquidBlock(DoAddonFluidProperties.RED_GRAPEJUICE, BlockBehaviour.Properties.copy(Blocks.WATER).mapColor(MapColor.COLOR_RED)));
     public static final RegistryEntry<Block> WHITE_GRAPEJUICE_BLOCK = BLOCKS.register("white_grapejuice", () -> new BotariumLiquidBlock(DoAddonFluidProperties.WHITE_GRAPEJUICE, BlockBehaviour.Properties.copy(Blocks.WATER).mapColor(MapColor.COLOR_LIGHT_GREEN)));
